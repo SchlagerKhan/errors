6da9ffcb10f7564018fafe538f8ea1825b23bf9f
Kalle Hansson <kh.hansson@gmail.com>
2019-07-07 16:26:09 +0200

    Added some more default errors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import { GenericError } from './generic';
 
 export { GenericError };
 
+/* NETWORK */
+export class NetworkError extends GenericError {}
+
+export class TimeoutError extends NetworkError {}
+export class ServerUnavailableError extends NetworkError {}
+export class ConnectionError extends NetworkError {}
+
+export class UrlNotFoundError extends NetworkError {}
+
 /* AUTH ERRORS */
 export class AuthError extends GenericError {}
 
@@ -10,3 +19,5 @@ export class UnauthorizedError extends AuthError {}
 
 /* OTHER */
 export class UnknownError extends GenericError {}
+
+// TODO: Refactor to monorepo and add generic-error as separate package
